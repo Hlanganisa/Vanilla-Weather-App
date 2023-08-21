5aae39845b565abf886be371d13ef4b3d3ab4ee8
Hlanganisa <141266919+Hlanganisa@users.noreply.github.com>
2023-08-21 10:31:54 +0200

    Added java-script structure

---
+++ src/app.js
@@ -0,0 +1,87 @@
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let dayIndex = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[dayIndex];
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "9159b2aab66d3679aa7c0c2915aadce4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+  }
+  function showTemperature(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.main.temp
+    );
+  
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].main;
+  }
+  
+  function search(event) {
+    event.preventDefault();
+    let cityElement = document.querySelector("#city");
+    let cityInput = document.querySelector("#city-input");
+    cityElement.innerHTML = cityInput.value;
+    searchCity(cityInput.value);
+  }
+  
+  function convertToCelsius(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = 30;
+  }
+  
+  function currentLocation(event) {
+    event.preventDefault();
+    let currentElement = document.querySelector("#time");
+    let currentInput = document.querySelector("#time-input");
+    currentElement.innerHTML = currentInput.value;
+  }
+  
+  let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
+  dateElement.innerHTML = formatDate(currentTime);
+  
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", search);
+  
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", convertToCelsius);
+  
+  let currentLocationButton = document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", currentLocation);
+  
+
+  let currentLocationButton = document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", currentLocation);
+
+let iconElement = document.querySelector("#icon")
+iconElement. setAttribute("src",
+'https://samples.openweathermap.org/data/2.5/weather?q=Mpumalanga&appid=b1b15e88fa797225412429c1c50c122a1'
+);
+
